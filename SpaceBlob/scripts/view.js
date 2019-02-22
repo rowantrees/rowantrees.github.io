261930ba171a0c9d32077f2443b5c7c0aff75137
thorsammer <forge@thorsammer.com>
2019-02-22 16:34:12 -0500

    testing some basics

---
+++ SpaceBlob/scripts/view.js
@@ -0,0 +1,14 @@
+function viewUniverse() {
+	document.getElementById('playSpace').style.backgroundColor='black';
+	
+}
+
+function viewGalaxy() {
+	document.getElementById('playSpace').style.backgroundColor='red';
+	
+}
+
+function viewPlanet() {
+	document.getElementById('playSpace').style.backgroundColor='blue';
+	
+}
